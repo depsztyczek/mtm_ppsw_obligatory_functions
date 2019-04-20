f7eb29ed2e60fbf4602b496080113fe257927d7d
unknown <dzialowy04@gmail.com>
2019-04-20 20:39:25 +0200

    Fucntion ready to verifiaction

---
+++ konwersje/UIntToHexStr.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define HEX_bm 0xF
+
+void UIntToHexStr(unsigned int uiValue, char pcStr[]){
+	
+	unsigned char ucNibbleCounter;
+	unsigned char ucCurrentNibble;
+	
+	pcStr[0]='0';
+	pcStr[1]='x';
+	for(ucNibbleCounter=0;ucNibbleCounter<4;ucNibbleCounter++){
+		ucCurrentNibble=((uiValue >> ucNibbleCounter*4) & HEX_bm);
+		if(ucCurrentNibble>9)
+			pcStr[5-ucNibbleCounter] = ucCurrentNibble - 10 + 'A';
+		else
+			pcStr[5-ucNibbleCounter] = ucCurrentNibble + '0';
+	}
+	pcStr[6]=NULL;
+}
+
